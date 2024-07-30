ddb9d409b92e6c3a9044f7dcedab073967fc021a
pejo85 <48516054+pejo85@users.noreply.github.com>
2024-07-30 23:16:22 +0400

    2

--- Junior-Programmer-Starter-Files/Assets/Scripts/UI/UIMainScene.cs
+++ Junior-Programmer-Starter-Files/Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,9 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void BackToManu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
